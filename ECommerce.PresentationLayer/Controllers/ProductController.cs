4334599b3666079284fd9a053cdf3385883ea5ef
Barış Bükümcüler <barisbukumculer@gmail.com>
2023-09-27 21:34:28 +0300

    ProductDetail and Designs of pages

--- ECommerce.PresentationLayer/Controllers/ProductController.cs
+++ ECommerce.PresentationLayer/Controllers/ProductController.cs
@@ -28,5 +28,17 @@ namespace ECommerce.PresentationLayer.Controllers
 			}
 			return View();
 		}
+		public async Task< IActionResult> ProductDetail(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7175/api/Product/GetProductById/" + id);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<ResultProductDto>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
 	}
 }
